0b71030e4e98223c9e53752bbc38cdd2378bbd2d
Zane Bradley <zanegbradley@gmail.com>
2016-08-09 18:31:15 -0700

    add Smartcar.java: main developer facing class

--- src/main/java/com/smartcar/sdk/Smartcar.java
+++ src/main/java/com/smartcar/sdk/Smartcar.java
@@ -1,48 +1,70 @@
 package com.smartcar.sdk;
 
-import java.io.IOException;
 import okhttp3.HttpUrl;
-import okhttp3.Credentials;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
-import okhttp3.FormBody;
 import com.google.gson.Gson;
 import java.util.ArrayList;
 
 final public class Smartcar {
 
-  final static String AUTH = "https://auth.smartcar.com/oauth/token";
-
-  private final String clientId, clientSecret, redirectUri;
+  private final String id, secret, redirectUri;
   private final String[] scope;
   private final Gson gson = new Gson();
+  private final AccessRequest access;
+  private String vehicleUrl;
 
-  public Smartcar(String clientId, String clientSecret,
+  public Smartcar(String id, String secret,
                   String redirectUri, String[] scope) {
-    this.clientId = clientId;
-    this.clientSecret = clientSecret;
+    this.id = id;
+    this.secret = secret;
     this.redirectUri = redirectUri;
     this.scope = scope;
+    this.access = new AccessRequest(id, secret);
   }
 
-  private Request makeAuthRequest(FormBody formBody){
-    return new Request.Builder()
-      .url(AUTH)
-      .header("Authorization", 
-              Credentials.basic(this.clientId, this.clientSecret))
-      .post(formBody)
-      .build();
+  void setAccessUrl(String url){
+    this.access.setUrl(url);
   }
 
+  void setVehicleUrl(String url){
+    this.vehicleUrl = url;
+  }
+
+  private VehicleRequest getVehicleRequest(String token){
+    VehicleRequest vehicleRequest = new VehicleRequest(token);
+    vehicleRequest.setUrl(this.vehicleUrl);
+    return vehicleRequest;
+  }
+
+  private ArrayList<Vehicle> getVehicleArray(String token, String json){
+    ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
+    ArrayList<String> vehicleIds = Util.getArray(json, "vehicles");
+    for (String vehicleId : vehicleIds){
+      Vehicle vehicle = getVehicle(token, vehicleId);
+      vehicles.add(vehicle);
+    }
+    return vehicles;
+  }
+
+  /**
+   * TODO add oauth state, and approval_prompt=[force|auto], and
+   * maybe change the parameter to just the OEM name
+   * 
+   * This link is what allows the user to login to their OEM account
+   * and accept your permissions defined in scope. If they accept, a
+   * GET request will be sent to your redirectUri with code and 
+   * state parameters
+   * 
+   * @param  baseUri 
+   * 
+   * @return oem authentication url
+   */
   public String getAuthUrl(String baseUri) {
     HttpUrl parsed = HttpUrl.parse(baseUri);
     if (parsed != null){
       return parsed.newBuilder()
         .addPathSegments("oauth/authorize")
         .addQueryParameter("response_type", "code")
-        .addQueryParameter("client_id", this.clientId)
+        .addQueryParameter("client_id", this.id)
         .addQueryParameter("redirect_uri", this.redirectUri)
         .addQueryParameter("scope", Util.join(this.scope))
         .build()
@@ -51,63 +73,70 @@ final public class Smartcar {
     return null;
   }
 
+  /**
+   * Exchange an authentication code for an Access object.
+   * 
+   * @param  code the retrieved authorization code
+   * 
+   * @return an <Access> containing an access_token and refresh_token
+   * 
+   * @throws <Exceptions.SmartcarException>
+   */
   public Access exchangeCode(String code) 
-    throws IOException {
-
-    FormBody formBody = new FormBody.Builder()
-      .add("grant_type", "authorization_code")
-      .add("code", code)
-      .add("redirect_uri", this.redirectUri)
-      .build();
-
-    String response = Util.call(makeAuthRequest(formBody));
-    return gson.fromJson(response, Access.class);
+  throws Exceptions.SmartcarException {
+    String json = this.access.code(code, this.redirectUri);
+    return gson.fromJson(json, Access.class);
   }
 
+  /**
+   * Exchange a refresh token for a new <Access> object
+   * @param  refreshToken
+   * @return an <Access> containing an access_token and refresh_token
+   * @throws <Exceptions.SmartcarException>
+   */
   public Access exchangeToken(String refreshToken) 
-    throws IOException {
+  throws Exceptions.SmartcarException {
 
-    FormBody formBody = new FormBody.Builder()
-      .add("grant_type", "refresh_token")
-      .add("refresh_token", refreshToken)
-      .build();
-
-    String response = Util.call(makeAuthRequest(formBody));
-    return gson.fromJson(response, Access.class);
+    String json = this.access.token(refreshToken);
+    return gson.fromJson(json, Access.class);
   }
   
-  public ArrayList<Vehicle> getVehicles(String accessToken){
-    Request request;
-    String response;
-    ArrayList<Vehicle> vehicles;
-    ArrayList<String> ids;
+  /**
+   * Get a list of the user's vehicles
+   * @param  token A valid Access Token
+   * @return The user's vehicles
+   * @throws Exceptions.SmartcarException
+   */
+  public ArrayList<Vehicle> getVehicles(String token)
+  throws Exceptions.SmartcarException {
 
-    /* https://api.smartcar.com/v1.0/vehicles */
-    request = new Request.Builder()
-      .url(Util.makeApiUrl())
-      .build();
-
-    /* 
-    {
-      "vehicles": [
-        "36ab27d0-fd9d-4455-823a-ce30af709ffc",
-        "3cd4ffb8-b0f6-45cf-9a4c-47d2102bb6b1"
-      ],
-      "paging": {
-        "count": 10,
-        "offset": 0
-      }
-    }
-    */
-    
-    // vehicleIds = ...
-    /*for (String vehicleId : vehicleIds ){
-
-    }*/
-    return null;
+    String json = getVehicleRequest(token).vehicles();
+    return getVehicleArray(token, json);
   }
 
-  public Vehicle getVehicle(String accessToken, String vehicleId){
-    return new Vehicle(accessToken, vehicleId);
+  /**
+   * Get a list of the user's vehicles
+   * @param  token  A valid Access Token
+   * @param  limit  Set the limit on the number of vehicles to return
+   *                [default: 10, maximum: 50]
+   * @param  offset Set the index to start the vehicle list at [default: 0]
+   */
+  public ArrayList<Vehicle> getVehicles(String token, int limit, int offset)
+  throws Exceptions.SmartcarException {
+
+    String json = getVehicleRequest(token).vehicles(limit, offset);
+    return getVehicleArray(token, json);
+  }
+
+  /**
+   * Get an user's vehicle
+   * @param  token     A valid Access Token
+   * @param  vehicleId A valid vehicle id
+   * @return           A new Vehicle object representing the user's vehicle
+   */
+  public Vehicle getVehicle(String token, String vehicleId){
+    Vehicle vehicle = new Vehicle(token, vehicleId);
+    if (this.vehicleUrl != null) vehicle.setUrl(this.vehicleUrl);
+    return vehicle;
   }
 }
