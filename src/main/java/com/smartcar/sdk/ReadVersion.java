17e718ce36ea02357216f92dddfcc5ed69ee8168
Robin Jayaswal <robin.jayaswal@smartcar.com>
2017-01-30 14:39:30 -0800

    remove unecessary error throwing

--- src/main/java/com/smartcar/sdk/ReadVersion.java
+++ src/main/java/com/smartcar/sdk/ReadVersion.java
@@ -19,11 +19,7 @@ public final class ReadVersion {
 
       inputStream = ReadVersion.class.getClassLoader().getResourceAsStream(propFileName);
 
-      if (inputStream != null) {
-        prop.load(inputStream);
-      } else {
-        throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-      }
+      prop.load(inputStream);
 
       result = prop.getProperty("VERSION");
 
