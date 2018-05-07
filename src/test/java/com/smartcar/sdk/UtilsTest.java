2e8447b5842a556fb1c03b00113f93944dac5727
Nick Williams <git@nickawilliams.com>
2018-05-07 14:48:39 -0700

    Added Utils test suite.

---
+++ src/test/java/com/smartcar/sdk/UtilsTest.java
@@ -0,0 +1,54 @@
+package com.smartcar.sdk;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Test Suite: Utils
+ */
+public class UtilsTest {
+  /**
+   * Verifies that join combines all array elements, delimited by the specifed
+   * separator.
+   */
+  @Test
+  public void testJoin() {
+    String separator = ", ";
+    String[] data = {
+        "unus",
+        "duo",
+        "tres",
+        "quattor"
+    };
+
+    String expected = "unus, duo, tres, quattor";
+
+    Assert.assertEquals(Utils.join(data, separator), expected);
+  }
+
+  /**
+   * Verifies that joining a single element results in only that element as a
+   * string with no usages of the specified separator.
+   */
+  @Test
+  public void testJoinWithOneElement() {
+    String separator = "::";
+    String[] data = {"qinque"};
+
+    String expected = "qinque";
+
+    Assert.assertEquals(Utils.join(data, separator), expected);
+  }
+
+  /**
+   * Verifies that joining an empty array results in an empty string.
+   */
+  @Test
+  public void testJoinWithEmptyArray() {
+    String separator = "-";
+    String[] data = {};
+    String expected = "";
+
+    Assert.assertEquals(Utils.join(data, separator), expected);
+  }
+}
