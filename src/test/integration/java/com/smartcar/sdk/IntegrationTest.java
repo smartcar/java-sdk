86d712511494b51360128398012c390dec10dab1
Nick Williams <git@nickawilliams.com>
2018-05-10 16:50:44 -0700

    Reorganized tests.

---
+++ src/test/integration/java/com/smartcar/sdk/IntegrationTest.java
@@ -0,0 +1,108 @@
+package com.smartcar.sdk;
+
+import org.testng.annotations.BeforeClass;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class IntegrationTest {
+  public enum Environment {
+    DEV,
+    QA,
+    STAGE,
+    PROD;
+  }
+
+  /**
+   * An extension of Properties that supports automatic environment variable
+   * expansion.
+   */
+  protected static class EnvProperties extends Properties {
+    /**
+     * Reads propertys from the input byte stream.
+     *
+     * @param inStream the input stream
+     *
+     * @throws IOException if an error occurred when reading the input stream
+     */
+    public synchronized void load(InputStream inStream) throws IOException {
+      // Load Properties
+      super.load(inStream);
+
+      // Expand Environment Variables
+      Set<String> stringKeys = this.stringPropertyNames();
+
+      for(String key : stringKeys) {
+        this.setProperty(key, EnvProperties.expandEnvVars(this.getProperty(key)));
+      }
+    }
+
+    /**
+     * Expand any environment variables embedded in the specified value.
+     *
+     * @param input the string containing possible environment variables
+     *
+     * @return the input with environment variable references expanded
+     */
+    private static String expandEnvVars(String input) {
+      if(input == null) {
+        return null;
+      }
+
+      Matcher matcher = Pattern.compile("\\$(\\{(\\w+)}|(\\w+))").matcher(input);
+      StringBuffer buffer = new StringBuffer();
+
+      while(matcher.find()) {
+        String envVarName = matcher.group(2) == null ? matcher.group(3) : matcher.group(2);
+        String envVarValue = System.getenv(envVarName);
+
+        matcher.appendReplacement(buffer, envVarValue == null ? "" : envVarValue);
+      }
+
+      matcher.appendTail(buffer);
+
+      return buffer.toString();
+    }
+  }
+
+  protected EnvProperties config = new EnvProperties();
+
+  /**srxzsxen
+   * Loads the configuration for the specified environment.
+   *
+   * @param env the desired environment
+   */
+  private void loadConfig(Environment env) throws IOException {
+    ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    InputStream stream = loader.getResourceAsStream("environment.qa.properties");
+
+//    InputStream in = this.getClass().getClassLoader().getResourceAsStream(
+//        "environment." + env.toString().toLowerCase() + ".properties");
+    this.config.load(stream);
+    stream.close();
+  }
+
+  /**
+   * Loads configuration before executing tests.
+   *
+   * @throws IOException if the configuration cannot be loaded
+   */
+  @BeforeClass
+  public void beforeClass() throws IOException {
+    String targetEnvString = System.getenv("INTEGRATION_TARGET_ENV");
+    Environment targetEnv;
+
+    if(targetEnvString == null) {
+      targetEnv = Environment.QA;
+    }
+    else {
+      targetEnv = Environment.valueOf(targetEnvString);
+    }
+
+    this.loadConfig(targetEnv);
+  }
+}
---
+++ src/test/unit/java/com/smartcar/sdk/VehicleTest.java
@@ -0,0 +1,37 @@
+package com.smartcar.sdk;
+
+import org.testng.annotations.Test;
+
+/**
+ * Test Suite: Vehicle
+ */
+public class VehicleTest {
+  @Test
+  public void testGetAuthUrl() {
+    String clientId = "66f498e1-9447-4333-a0c2-1889909c1b44";
+    String clientSecret = "aef2d2c8-5674-44de-b852-5b4aee143cd5";
+    String redirectUri = "http://localhost:4000/auth";
+    String state = "foobar";
+
+    AuthClient authClient = new AuthClient(clientId, clientSecret, redirectUri, true);
+
+//    Assert.assertEquals(authClient.getAuthUrl(state), "");
+
+    String code = "21b0f3c0-6f29-431f-ad99-6180d56e2c4f";
+
+    Auth auth = null;
+    try {
+      auth = authClient.exchangeCode(code);
+    } catch (SmartcarException ex) {
+      System.out.println("Code: " + ex.getStatus());
+      System.out.println("Message: " + ex.getMessage());
+      ex.printStackTrace();
+    }
+
+//    Assert.assertEquals(auth.getAccessToken(), "");
+
+//    String accessToken = auth.getAccessToken();
+
+
+  }
+}
