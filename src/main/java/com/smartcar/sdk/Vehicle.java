090e7b2ee091dd31333944c3de865d601dd459a2
Greg Chan <gregorywarrenchan@gmail.com>
2018-07-10 15:44:45 -0700

    Move JSON MediaType to ApiClient

--- src/main/java/com/smartcar/sdk/Vehicle.java
+++ src/main/java/com/smartcar/sdk/Vehicle.java
@@ -9,14 +9,11 @@ import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.Request;
 import okhttp3.RequestBody;
-import okhttp3.MediaType;
 
 /**
  * Smartcar Vehicle API Object
  */
 public class Vehicle extends ApiClient {
-  public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-
   public enum UnitSystem {
     DEFAULT,
     IMPERIAL,
