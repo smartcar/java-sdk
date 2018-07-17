fbeb2ff9dd701137265d395d55fb4b06da8a2311
Nick Williams <git@nickawilliams.com>
2018-07-17 15:07:24 -0700

    Added integration test coverage for AuthClient static methods.

--- src/integration/java/com/smartcar/sdk/AuthTest.java
+++ src/integration/java/com/smartcar/sdk/AuthTest.java
@@ -1,6 +1,7 @@
 package com.smartcar.sdk;
 
 import com.smartcar.sdk.data.Auth;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -11,6 +12,19 @@ import static org.testng.Assert.assertNotNull;
  * Integration Test Suite: Authentication
  */
 public class AuthTest extends IntegrationTest {
+    private String accessToken;
+
+    /**
+     * Obtains a valid access token to be used with tests in this suite.
+     *
+     * @throws Exception if a valid access token cannot be obtained
+     */
+    @BeforeSuite
+    public void beforeSuite() throws Exception {
+        Auth auth = this.getAuth();
+        this.accessToken = auth.getAccessToken();
+    }
+
     /**
      * Tests that the OAuth 2.0 flow succeeds and results in a valid auth code.
      *
@@ -22,5 +36,25 @@ public class AuthTest extends IntegrationTest {
         Auth authToken = this.getAuth();
 
         assertNotNull(authToken.getAccessToken());
+    }
+
+    /**
+     * Tests that a valid user ID can be obtained.
+     *
+     * @throws SmartcarException if the request fails
+     */
+    @Test
+    public void testGetUserId() throws SmartcarException {
+        AuthClient.getUserId(this.accessToken);
+    }
+
+    /**
+     * Tests that a valid list of vehicles can be obtained.
+     *
+     * @throws SmartcarException if the request fails
+     */
+    @Test
+    public void testGetVehicleIds() throws SmartcarException {
+        AuthClient.getVehicleIds(this.accessToken);
     }
 }
