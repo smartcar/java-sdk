2578efa50c364793a8829228b15815918e2278be
Jeremy Zhang <jeremy.zhang@smartcar.com>
2019-02-23 17:56:38 -0800

    add javadoc

--- src/main/java/com/smartcar/sdk/AuthClient.java
+++ src/main/java/com/smartcar/sdk/AuthClient.java
@@ -359,7 +359,7 @@ public class AuthClient extends ApiClient {
   /**
    * Determine if a vehicle is compatible with Smartcar.
    * 
-   * @param compatibility
+   * @param compatibility whether the vehicle is compatible with Smartcar
    * 
    * @return true if the vehicle is compatible
    * 
