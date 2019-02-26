b85c9696882b241d496e5536a894894e5413b8ab
Jeremy Zhang <jeremy.zhang@smartcar.com>
2019-02-25 19:39:30 -0800

    replace tab

--- src/main/java/com/smartcar/sdk/data/Compatibility.java
+++ src/main/java/com/smartcar/sdk/data/Compatibility.java
@@ -4,34 +4,34 @@ package com.smartcar.sdk.data;
  * A container for the compatibility endpoint
  */
 public class Compatibility extends ApiData {
-	private boolean compatible;
+  private boolean compatible;
 
-	/**
-	 * Initializes a new instance of Compatibility
-	 *
-	 * @param compatible whether the vehicle is compatible with Smartcar
-	 */
-	public Compatibility(final boolean compatible) {
-		this.compatible = compatible;
-	}
+  /**
+   * Initializes a new instance of Compatibility
+   *
+   * @param compatible whether the vehicle is compatible with Smartcar
+   */
+  public Compatibility(final boolean compatible) {
+    this.compatible = compatible;
+  }
 
-	/**
-	 * Return the compatible variable
-	 *
-	 * @return compatible
-	 */
-	public boolean getCompatible() {
-		return this.compatible;
-	}
+  /**
+   * Return the compatible variable
+   *
+   * @return compatible
+   */
+  public boolean getCompatible() {
+    return this.compatible;
+  }
 
-	/**
-	 * Sets the compatible variable
-	 *
-	 * @param compatible whether the vehicle is compatible with Smartcar
-	 */
-	public void setCompatible(boolean compatible) {
-		this.compatible = compatible;
-	}
+  /**
+   * Sets the compatible variable
+   *
+   * @param compatible whether the vehicle is compatible with Smartcar
+   */
+  public void setCompatible(boolean compatible) {
+    this.compatible = compatible;
+  }
 
   /**
    * Return the string representation
