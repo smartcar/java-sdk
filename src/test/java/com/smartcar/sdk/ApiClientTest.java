3209ba35aeb288f87d70e8195b2b45d79212afd7
Theodore Wahle <theodorewahle@gmail.com>
2019-09-25 18:36:02 -0700

    fix: made custom lambda to allow gson to process camel case (#68)
    fixed docs
    
    used library function to make the code cleaner
    
    test to check for proper key parsing

--- src/test/java/com/smartcar/sdk/ApiClientTest.java
+++ src/test/java/com/smartcar/sdk/ApiClientTest.java
@@ -1,7 +1,13 @@
 package com.smartcar.sdk;
 
+import com.smartcar.sdk.data.*;
+import com.smartcar.sdk.data.VehicleOil;
+import com.smartcar.sdk.data.ApiData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import com.smartcar.sdk.data.*;
+import javax.json.JsonObject;
+import javax.json.Json;
 
 /**
  * Test Suite: ApiClient
@@ -13,5 +19,13 @@ public class ApiClientTest {
     // version has to be null because the package isn't built yet
     String regex = "^Smartcar/DEVELOPMENT \\((\\w+); (\\w+)\\) Java v(\\d+\\.\\d+\\.\\d_\\d+) .*";
     Assert.assertTrue(ApiClient.USER_AGENT.matches(regex));
+  }
+
+  @Test
+  public void testKeyParsing() {
+    String testData = "{\"lifeRemaining\":0.86}";
+
+    VehicleOil data = ApiClient.gson.create().fromJson(testData, VehicleOil.class);
+    Assert.assertTrue(data.getLifeRemaining() == 0.86);
   }
 }
