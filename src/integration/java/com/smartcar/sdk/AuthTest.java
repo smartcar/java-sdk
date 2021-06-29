fd6f9eb75a6f3b7f383591ccf50dab281d4ee88e
Gurpreet Atwal <git@gatwal.com>
2021-06-29 16:53:47 -0700

    refactor: clean up integration tests

--- src/integration/java/com/smartcar/sdk/AuthTest.java
+++ src/integration/java/com/smartcar/sdk/AuthTest.java
@@ -1,64 +1,42 @@
 package com.smartcar.sdk;
 
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertNotEquals;
-import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertTrue;
-
 import com.smartcar.sdk.data.Auth;
-import com.smartcar.sdk.data.SmartcarResponse;
-import com.smartcar.sdk.data.VehicleIds;
-import java.net.MalformedURLException;
+import com.smartcar.sdk.helpers.AuthHelpers;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-/** Integration Test Suite: Authentication */
-public class AuthTest extends IntegrationTest {
-  private static final String PATTERN_UUID =
-      "[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}";
+import static org.testng.Assert.assertNotNull;
 
-  private String accessToken;
+public class AuthTest {
+  private AuthClient client;
+  private String authorizeUrl;
 
-  /**
-   * Obtains a valid access token to be used with tests in this suite.
-   *
-   * @throws Exception if a valid access token cannot be obtained
-   */
   @BeforeSuite
-  public void beforeSuite() throws Exception {
-    Auth auth = this.getAuth();
-    this.accessToken = auth.getAccessToken();
+  public void beforeSuite()  {
+    this.client = AuthHelpers.getConfiguredAuthClientBuilder().build();
+    this.authorizeUrl = client.authUrlBuilder(new String[] {"read_vehicle_info"}).build();
   }
 
-  /**
-   * Tests that the OAuth 2.0 flow succeeds and results in a valid auth code.
-   *
-   * @throws MalformedURLException when a URL cannot be parsed during testing
-   * @throws Exception when an auth code cannot be obtained
-   */
   @Test
-  public void testAuthFlowObtainsValidAuthTokens() throws Exception {
-    Auth authToken = this.getAuth();
+  public void testExchangeCode() throws SmartcarException {
+    String code = AuthHelpers.runAuthFlow(this.authorizeUrl);
+    Auth access = client.exchangeCode(code);
 
-    assertNotNull(authToken.getAccessToken());
-    assertNotNull(authToken.getRefreshToken());
-    assertNotNull(authToken.getExpiration());
-    assertNotNull(authToken.getRefreshExpiration());
+    assertNotNull(access.getAccessToken());
+    assertNotNull(access.getRefreshToken());
+    assertNotNull(access.getExpiration());
+    assertNotNull(access.getRefreshExpiration());
   }
 
-  /**
-   * Tests that a valid refresh token can be used to obtain new credentials.
-   *
-   * @throws Exception if new credentials cannot be obtained
-   */
   @Test
-  public void testRefreshToken() throws Exception {
-    Auth oldAuth = this.getAuth();
-    Auth newAuth = this.authClient.exchangeRefreshToken(oldAuth.getRefreshToken());
+  public void testExchangeRefreshToken() throws SmartcarException {
+    String code = AuthHelpers.runAuthFlow(this.authorizeUrl);
+    Auth oldAccess = client.exchangeCode(code);
+    Auth newAccess = client.exchangeRefreshToken(oldAccess.getRefreshToken());
 
-    assertNotNull(newAuth.getAccessToken());
-    assertNotNull(newAuth.getRefreshToken());
-    assertNotNull(newAuth.getExpiration());
-    assertNotNull(newAuth.getRefreshExpiration());
+    assertNotNull(newAccess.getAccessToken());
+    assertNotNull(newAccess.getRefreshToken());
+    assertNotNull(newAccess.getExpiration());
+    assertNotNull(newAccess.getRefreshExpiration());
   }
 }
