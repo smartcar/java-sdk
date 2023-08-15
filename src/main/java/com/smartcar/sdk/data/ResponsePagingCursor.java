d1a6b2baa3dfaf25dd3702ba1ac89f320cfea0e5
nbry <nwbryant@umich.edu>
2023-08-15 08:52:48 -0700

    chore: update response paging

--- src/main/java/com/smartcar/sdk/data/ResponsePagingCursor.java
+++ src/main/java/com/smartcar/sdk/data/ResponsePagingCursor.java
@@ -2,17 +2,7 @@ package com.smartcar.sdk.data;
 
 /** POJO for the response paging w/cursor object */
 public class ResponsePagingCursor extends ApiData {
-    private int count;
     private String cursor;
-
-    /**
-     * Returns the response count
-     *
-     * @return response count
-     */
-    public int getCount() {
-        return this.count;
-    }
 
     /**
      * Returns the response cursor
@@ -26,6 +16,6 @@ public class ResponsePagingCursor extends ApiData {
     /** @return a stringified representation of ResponsePagingCursor */
     @Override
     public String toString() {
-        return this.getClass().getName() + "{" + "count=" + count + ", cursor=" + cursor + '}';
+        return this.getClass().getName() + "{" + "cursor=" + cursor + '}';
     }
 }
