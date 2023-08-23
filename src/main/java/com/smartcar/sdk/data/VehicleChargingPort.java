1cb1add99d67654bf9335a42fcf1bdaaf1c05dc0
Naomi Perez <naomisperez99@gmail.com>
2023-08-23 15:29:06 -0700

    add VehicleChargingPort class
    charging port fix
    
    charging port fix

---
+++ src/main/java/com/smartcar/sdk/data/VehicleChargingPort.java
@@ -0,0 +1,32 @@
+package com.smartcar.sdk.data;
+
+public class VehicleChargingPort {
+    private String type;
+    private String status;
+
+    /**
+     * Returns the charging port type
+     *
+     * @return charging port type
+     */
+    public String getType() {
+        return this.type;
+    }
+
+    /**
+     * Returns the charging port status
+     *
+     * @return charging port status
+     */
+    public String getStatus() {
+        return this.status;
+    }
+
+    /**
+     * @return a stringified representation of VehicleChargingPort
+     */
+    @Override
+    public String toString() {
+        return this.getClass().getName() + "{" + "type=" + type + ", status=" + status + '}';
+    }
+}
