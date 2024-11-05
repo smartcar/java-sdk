7c4accb54619a0bffcf33fa2d263795384710e2c
Haig (Hike) Hovsepian <hhovsepi@gmail.com>
2024-11-05 14:30:21 -0600

    address feedback

--- src/main/java/com/smartcar/sdk/data/DiagnosticSystem.java
+++ src/main/java/com/smartcar/sdk/data/DiagnosticSystem.java
@@ -1,16 +1,9 @@
 package com.smartcar.sdk.data;
 
-import com.google.gson.annotations.SerializedName;
-
 /** Data class for an individual diagnostic system. */
 public class DiagnosticSystem {
-    @SerializedName("systemId")
     private String systemId;
-
-    @SerializedName("status")
     private String status;
-
-    @SerializedName("description")
     private String description;
 
     /** No-argument constructor for deserialization. */
@@ -19,31 +12,16 @@ public class DiagnosticSystem {
 
     /** Gets the system ID. */
     public String getSystemId() {
-        return systemId;
-    }
-
-    /** Sets the system ID. */
-    public void setSystemId(String systemId) {
-        this.systemId = systemId;
+        return this.systemId;
     }
 
     /** Gets the status of the system. */
     public String getStatus() {
-        return status;
-    }
-
-    /** Sets the status of the system. */
-    public void setStatus(String status) {
-        this.status = status;
+        return this.status;
     }
 
     /** Gets the description of the system. */
     public String getDescription() {
-        return description;
-    }
-
-    /** Sets the description of the system. */
-    public void setDescription(String description) {
-        this.description = description;
+        return this.description;
     }
 }
--- src/main/java/com/smartcar/sdk/data/VehicleDiagnosticTroubleCodes.java
+++ src/main/java/com/smartcar/sdk/data/VehicleDiagnosticTroubleCodes.java
@@ -1,11 +1,9 @@
 package com.smartcar.sdk.data;
 
-import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /** Data class for the diagnostic trouble codes of a vehicle. */
 public class VehicleDiagnosticTroubleCodes extends ApiData {
-    @SerializedName("activeCodes")
     private List<DiagnosticTroubleCode> activeCodes;
 
     /** No-argument constructor for deserialization. */
@@ -14,11 +12,6 @@ public class VehicleDiagnosticTroubleCodes extends ApiData {
 
     /** Returns the list of active diagnostic trouble codes. */
     public List<DiagnosticTroubleCode> getActiveCodes() {
-        return activeCodes;
-    }
-
-    /** Sets the list of active diagnostic trouble codes. */
-    public void setActiveCodes(List<DiagnosticTroubleCode> activeCodes) {
-        this.activeCodes = activeCodes;
+        return this.activeCodes;
     }
 }
