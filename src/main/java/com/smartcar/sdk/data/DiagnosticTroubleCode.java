7268ed677e6aba2ae8340fb619be9e31344f7554
Haig (Hike) Hovsepian <hhovsepi@gmail.com>
2024-11-05 15:10:46 -0600

    finish removing set methods

--- src/main/java/com/smartcar/sdk/data/DiagnosticTroubleCode.java
+++ src/main/java/com/smartcar/sdk/data/DiagnosticTroubleCode.java
@@ -1,13 +1,9 @@
 package com.smartcar.sdk.data;
 
-import com.google.gson.annotations.SerializedName;
 
 /** Data class for an individual diagnostic trouble code. */
 public class DiagnosticTroubleCode {
-    @SerializedName("code")
     private String code;
-
-    @SerializedName("timestamp")
     private String timestamp;
 
     /** No-argument constructor for deserialization. */
@@ -16,21 +12,11 @@ public class DiagnosticTroubleCode {
 
     /** Gets the diagnostic trouble code. */
     public String getCode() {
-        return code;
-    }
-
-    /** Sets the diagnostic trouble code. */
-    public void setCode(String code) {
-        this.code = code;
+        return this.code;
     }
 
     /** Gets the timestamp associated with the code. */
     public String getTimestamp() {
-        return timestamp;
-    }
-
-    /** Sets the timestamp associated with the code. */
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
+        return this.timestamp;
     }
 }
--- src/main/java/com/smartcar/sdk/data/VehicleDiagnosticSystemStatus.java
+++ src/main/java/com/smartcar/sdk/data/VehicleDiagnosticSystemStatus.java
@@ -1,11 +1,9 @@
 package com.smartcar.sdk.data;
 
-import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /** Data class for the diagnostic system status of a vehicle. */
 public class VehicleDiagnosticSystemStatus extends ApiData {
-    @SerializedName("systems")
     private List<DiagnosticSystem> systems;
 
     /** No-argument constructor for deserialization. */
@@ -14,11 +12,6 @@ public class VehicleDiagnosticSystemStatus extends ApiData {
 
     /** Returns the list of diagnostic systems. */
     public List<DiagnosticSystem> getSystems() {
-        return systems;
-    }
-
-    /** Sets the list of diagnostic systems. */
-    public void setSystems(List<DiagnosticSystem> systems) {
-        this.systems = systems;
+        return this.systems;
     }
 }
