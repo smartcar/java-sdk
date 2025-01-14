0e306753934896a956f03fbce1ac13d0d5977db0
bdr99 <2292715+bdr99@users.noreply.github.com>
2025-01-14 15:25:15 +0000

    Deploying to gh-pages from @ smartcar/java-sdk@72e0f6105f1a629d8b9f754a313de40885d21ecc 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.smartcar.sdk"},{"l":"com.smartcar.sdk.data"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.smartcar.sdk"},{"l":"com.smartcar.sdk.data"},{"l":"com.smartcar.sdk.deserializer"}]
