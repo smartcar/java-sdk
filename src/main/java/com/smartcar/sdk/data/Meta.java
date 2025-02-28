930f7b74fe22d3ab69c54fb78ad58dbd3bef073c
Leonel <leonel@Leonels-MacBook-Pro.local>
2025-02-28 05:14:59 -0300

    remove comments

--- src/main/java/com/smartcar/sdk/data/Meta.java
+++ src/main/java/com/smartcar/sdk/data/Meta.java
@@ -10,39 +10,21 @@ import java.time.Instant;
 import java.util.Date;
 import java.util.TimeZone;
 
-/**
- * A container for the metadata in the response from a Smartcar API request.
- */
 public class Meta {
-    /** Unique identifier for the API request that can be used for debugging */
     @SerializedName("sc-request-id")
     private String requestId;
     
-    /** ISO 8601 timestamp of when the data was retrieved from the vehicle */
     @SerializedName("sc-data-age")
     private String dataAge = null;
     
-    /** Unit system used in vehicle data (imperial or metric) */
     @SerializedName("sc-unit-system")
     private String unitSystem;
     
-    /** ISO 8601 timestamp of when the data was fetched by Smartcar */
     @SerializedName("sc-fetched-at")
     private String fetchedAt = null;
 
-    /**
-     * Returns the request ID.
-     *
-     * @return the request ID
-     */
     public String getRequestId() { return this.requestId; }
 
-    /**
-     * Returns the data age as a Date.
-     *
-     * @return the data age
-     * @throws SmartcarException if there is a parsing error
-     */
     public Date getDataAge() throws SmartcarException {
         if (this.dataAge == null) {
             return null;
@@ -56,19 +38,8 @@ public class Meta {
         }
     }
 
-    /**
-     * Returns the unit system.
-     *
-     * @return the unit system
-     */
     public String getUnitSystem() { return this.unitSystem; }
     
-    /**
-     * Returns the fetched at timestamp as a Date.
-     *
-     * @return the fetched at timestamp
-     * @throws SmartcarException if there is a parsing error
-     */
     public Date getFetchedAt() throws SmartcarException {
         if (this.fetchedAt == null) {
             return null;
